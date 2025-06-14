deb5f2ca2ba50802d999b544fe1d3e009074d7c1
programmer-sjk <wjdrnr6662@gmail.com>
2025-06-14 13:38:31 +0900

    feat: 태그 덮어쓰기 가능으로 설정하고 테스트

--- myapp/src/app.service.ts
+++ myapp/src/app.service.ts
@@ -31,6 +31,6 @@ export class AppService {
 
   getMessage(): string {
     console.log('noooooooo??');
-    return 'Hi~~~~~~ ECS CI/CD Rollback Test!!!!';
+    return 'Hi~~~~~~ ECS CI/CD Rollback Test!!!!!';
   }
 }
