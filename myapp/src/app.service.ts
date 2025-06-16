4b96f295e76b90a9ea3dbc8bac19a388b31cb40d
programmer-sjk <wjdrnr6662@gmail.com>
2025-06-16 11:54:00 +0900

    feat: 테스트용 aws 코드 및 import 제거

--- myapp/src/app.service.ts
+++ myapp/src/app.service.ts
@@ -1,28 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import {
-  Route53Client,
-  CreateHostedZoneCommand,
-  ChangeResourceRecordSetsCommand,
-  Change,
-  ListHostedZonesByNameCommand,
-} from '@aws-sdk/client-route-53'; // ES Modules import
 import { ConfigService } from '@nestjs/config';
-import {
-  ACMClient,
-  RequestCertificateCommand,
-  GetCertificateCommand,
-  DescribeCertificateCommand,
-  UpdateCertificateOptionsCommand,
-} from '@aws-sdk/client-acm';
-import {
-  ElasticLoadBalancingV2Client,
-  AddListenerCertificatesCommand,
-} from '@aws-sdk/client-elastic-load-balancing-v2';
 
 @Injectable()
 export class AppService {
-  private readonly region = 'ap-northeast-2';
-
   constructor(private readonly configService: ConfigService) {}
 
   getHello(): string {
